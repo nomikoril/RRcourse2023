336afed51c2525e7e40fa82336e14224a953b422
Nomin Batbayar <100386867+nomikoril@users.noreply.github.com>
2023-06-01 13:18:11 +0200

    changes

--- Quarto and MD 3/4. Params/Codes and instructions.R
+++ Quarto and MD 3/4. Params/Codes and instructions.R
@@ -13,11 +13,11 @@ quarto_render("QMD_class_3_4.qmd", execute_params = list(
 reg <- "Asia"
 y <- 2049
 
-quarto_render("QMD_class_3_4.qmd", execute_params = list(
+quarto_render("C:/Users/nomin/OneDrive/Desktop/DSBA/4.2. Reproducible Research/Lab 4/RRcourse2023/Quarto and MD 3/4. Params/QMD_class_3_4.qmd", execute_params = list(
   year = y,
   region = reg,
   printcode = FALSE,
   data = "file.csv"
-), output_file = paste0("Report-", reg, "-", y, ".html"))
+), output_file = paste0("Game of Thrones Report-", "Season-", y, ".html"))
 
 
